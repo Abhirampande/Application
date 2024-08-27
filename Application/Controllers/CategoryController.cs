882688a477f6ae4a7a46cf5b04480d7d5abccd39
Abhiram <abhirampandey54@gmail.com>
2024-08-27 22:28:38 +0530

    Ui updated

--- Application/Controllers/CategoryController.cs
+++ Application/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace Application.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
