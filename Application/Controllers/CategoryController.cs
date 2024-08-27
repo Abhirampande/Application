283e2d1671dad7ad6c4708ccde59412838c78f3e
Abhiram <abhirampandey54@gmail.com>
2024-08-27 12:31:43 +0530

    html and table created

--- Application/Controllers/CategoryController.cs
+++ Application/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Data;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
