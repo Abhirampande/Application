2eee07669604ca97e7f5058d4e712464388180a3
Abhiram <abhirampandey54@gmail.com>
2024-08-30 10:42:17 +0530

    save into database added

--- Application/Controllers/CategoryController.cs
+++ Application/Controllers/CategoryController.cs
@@ -14,12 +14,20 @@ namespace Application.Controllers
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList(); // check all the categories
             return View(objCategoryList);
         }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            _db.Categories.Add(obj); //keeps the record of all changes
+            _db.SaveChanges(); //it will create the category
+            return RedirectToAction("Index");// to go to a different controller, write controller name
         }
     }
 }
